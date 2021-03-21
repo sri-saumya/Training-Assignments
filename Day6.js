1f8273317c0b064055bcf76000d573b8be2ab078
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-03-21 12:06:34 -0700

    for of loop ,to iterate object properties

---
+++ Day6.js
@@ -0,0 +1,9 @@
+var student = {
+    name : 'Saumya',
+    rollno : '01',
+    stream : 'CSE'
+
+};
+console.log(Object.keys(student));
+console.log(Object.values(student));
+console.log(Object.entries(student));

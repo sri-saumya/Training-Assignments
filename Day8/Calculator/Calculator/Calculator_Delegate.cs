943171d038248f53fddb51b65407a97999e8f691
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-03-24 20:32:31 -0700

    Calculator

---
+++ Day8/Calculator/Calculator/Calculator_Delegate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calculator_Delegate
+    {
+        public delegate int cal_delegate(int num1, int num2);
+        public void result(cal_delegate cd , int n1, int n2)
+        {
+            var ans = cd(n1, n2);
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ Day8/Calculator/Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator_Delegate cd = new Calculator_Delegate();
+            Calculator_Operations co = new Calculator_Operations();
+            Calculator_Delegate.cal_delegate d1 = co.div;
+            cd.result(d1, 10, 0);
+        }
+    }
+}

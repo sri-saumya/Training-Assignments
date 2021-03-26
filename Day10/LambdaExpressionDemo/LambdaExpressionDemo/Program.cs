fb678e4e766ce75e3ace44ded02145fc0dc3e047
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-03-26 01:49:59 -0700

    LambdaExpressionDemo

---
+++ Day10/LambdaExpressionDemo/LambdaExpressionDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LambdaExpressionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Lambda Expresion !!");
+            Func<int, int> square = (x) => x * x;
+            System.Linq.Expressions.Expression<Func<int, int>> e = x => x * x;
+            var a = e.Compile();
+
+            Console.WriteLine(a(6));
+        }
+    }
+}

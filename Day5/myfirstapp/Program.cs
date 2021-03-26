6f8999d919e45ec494aae8bdaf5381144fea3a12
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-03-26 03:31:43 -0700

    c#

---
+++ Day5/myfirstapp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace myfirstapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name : ");
+
+            var date = DateTime.Now;
+            Console.WriteLine($"Hello {name} on {date}");
+            Console.ReadKey(true);
+        }
+    }
+}

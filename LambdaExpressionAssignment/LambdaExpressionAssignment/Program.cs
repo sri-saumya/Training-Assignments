e913b3a8c85f4b420ac6a2db2f8882e0f200b8c9
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-03-26 05:03:40 -0700

    LambdaExpressionAssignment

---
+++ LambdaExpressionAssignment/LambdaExpressionAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace LambdaExpressionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find word in paragraph");
+            string paragraph = "Coding is basically to the computer language " +
+                "used to develop apps, websites, and software. Without it, we’d" +
+                " have none of the to most popular technology we’ve come to rely on such " +
+                "as Facebook, our smartphones the browser we choose to view our" +
+                " favorite blogs, or even the blogs themselves. It all runs on code.";
+
+            string word = "to";
+            
+            var arr = paragraph.Split(" ");
+
+            var count = (from i in arr where i == word select i).ToList();
+            Console.WriteLine("Lambda expression : ");
+            Console.WriteLine(count.Count());
+
+            var count2 = arr.Where(x => x==word);
+            foreach (string element in count2)
+            {
+                Console.WriteLine($"Element : {element}");
+
+            }
+            Console.WriteLine("Linq : ");
+            Console.WriteLine(count2.Count());
+
+
+
+        }
+    }
+}

ba920ae689e2fda63c7eac3c2857897d3674fe6c
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-04-07 08:08:36 -0700

    c

--- databindingap/src/app/mycustomepipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MycustomepipePipe } from './mycustomepipe.pipe';
-
-describe('MycustomepipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new MycustomepipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- databindingap/src/app/pricepipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PricepipePipe } from './pricepipe.pipe';
-
-describe('PricepipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PricepipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
